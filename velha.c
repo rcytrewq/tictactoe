23b8073872cbe617e88349456847a1b3ec598da8
Yuri Correa <correa.yuri@engenharia.ufjf.br>
2021-05-21 17:08:21 -0300

    possible to play

--- velha.c
+++ velha.c
@@ -37,33 +37,33 @@ void assemble(){
     return;
 }
 
-void result(){
+int result(){
     for (i=0; i<dim; i++){
         if(matrix[i][0]== matrix[i][1] && matrix[i][0] == matrix[i][2] && matrix[i][0] != 0){
             printf("\t\tFim de Jogo!\nJogador %d venceu!", matrix[i][0]);
         
-        break;
+        return 1;
         }
 
         if(matrix[0][i]== matrix[1][i] && matrix[0][i] == matrix[2][i] && matrix[0][i] != 0){
             printf("\t\tFim de Jogo!\nJogador %d venceu!", matrix[0][i]);
         
-        break;
+        return 1;
         }
 
         if(matrix[1][1]== matrix[2][2] && matrix[1][1] == matrix[0][0] && matrix[0][0] != 0){
             printf("\t\tFim de Jogo!\nJogador %d venceu!", matrix[0][0]);
         
-        break;
+        return 1;
         }
 
         if(matrix[0][2]== matrix[1][1] && matrix[1][1] == matrix[2][0] && matrix[1][1] != 0){
-            printf("\t\tFim de Jogo!\nJogador %d venceu!", matrix[1][1]);
+            printf("\t\tFim de Jogo!\nJogador %d venceu!\n\n", matrix[1][1]);
         
-        break;
+        return 1;
         }
     }
-    return;
+    return 0;
 
 }
 
@@ -77,7 +77,7 @@ int check(int linha, int coluna){
 
 void player1(){
     valid = 0;
-    printf("\t\tJOGADOR 1, eh a sua vez de jogar!\n\t\tVoce deve inserir a linha e a coluna em que deseja jogar\n\n");
+    printf("\n\n\t\tJOGADOR 1, eh a sua vez de jogar!\n\t\tVoce deve inserir a linha e a coluna em que deseja jogar\n\n");
     
     while (valid!=1){
         printf("\t\tInsira a linha: ");
@@ -91,17 +91,62 @@ void player1(){
     return;
 }
 
-int main(){
-    for (i=0; i<dim; i++){
-        for (j=0; j<dim; j++){
-            if(j+i==2) matrix[i][j] = 1;
-            else matrix[i][j] = 0;
+void player2(){
+    valid = 0;
+    printf("\n\n\t\tJOGADOR 2, eh a sua vez de jogar!\n\t\tVoce deve inserir a linha e a coluna em que deseja jogar\n\n");
+    
+    while (valid!=1){
+        printf("\t\tInsira a linha: ");
+        scanf("%d", &linha);
+        printf("\t\tInsira a coluna: ");
+        scanf("%d", &coluna);
+        valid = check(linha-1, coluna-1);
     }
     
+    matrix[linha-1][coluna-1] = 2;
+    return;
 }
+
+
+void replaymenu(){
+    return;
+}
+
+void matrixreset(){
+    for (i=0; i<dim; i++){
+        for (j=0; j<dim; j++){
+            matrix[i][j] = 0;            
+        }   
+    
+    }
+}
+
+void game(){
+    matrixreset();
+    //sorteio de quem começa
     assemble();
-    player1();
-    assemble();
-    result();
+    while(1==1){
+        player1();
+        assemble();
+        if (result()==1){
+            replaymenu();
+            break;
+        }
+        player2();
+        assemble();
+        if (result()==1){
+            replaymenu();
+            break;
+        }
+    }
+
+}
+
+
+int main(){
+    
+
+    game();
+    
     return 0;
 }
